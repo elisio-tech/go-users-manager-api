b92f714a3a68ee1cb4795dc53b968efcf9b0b748
Elisio Augusto <elisiouiux@gmail.com>
2026-03-24 20:42:54 +0100

    findall

--- internal/repository/user_Repository_impl.go
+++ internal/repository/user_Repository_impl.go
@@ -18,3 +18,11 @@ func (u *GormUserRepository) Create(usr *models.User) error {
 	return u.db.Create(usr).Error
 }
 
+func (u *GormUserRepository) FindAll() ([]models.User, error) {
+	var usr []models.User
+
+	if err := u.db.Find(&usr).Error; err != nil {
+		return nil, err
+	}
+	return usr, nil
+}
