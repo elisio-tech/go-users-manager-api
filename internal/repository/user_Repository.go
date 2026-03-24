b12917ad556323cd27778ad38f032b944cd2d5de
Elisio Augusto <elisiouiux@gmail.com>
2026-03-24 22:42:40 +0100

    getUsers

--- internal/repository/user_Repository.go
+++ internal/repository/user_Repository.go
@@ -4,7 +4,7 @@ import "go-users-manager-api/internal/models"
 
 // define o contrato quais metodos o user pode ter
 type UserRepository interface {
-	List() (*models.User, error)
+	List() ([]models.User, error)
 	Create(user *models.User) error
 	FindByID(id string) (*models.User, error)
 	Update(id string, usr *models.User) error
--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,2 +1,22 @@
 package service
 
+import (
+	"go-users-manager-api/internal/models"
+	"go-users-manager-api/internal/repository"
+)
+
+type UserService struct {
+	UserRepo repository.UserRepository
+}
+
+func NewUserService(repo repository.UserRepository) *UserService {
+	return &UserService{UserRepo: repo}
+}
+
+func (u *UserService) GetUsers() ([]models.User, error) {
+	users, err := u.UserRepo.List()
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
+}
