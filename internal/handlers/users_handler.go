10bc37febab2841ba47f05e7c68d0b0e36e9fa68
Elisio Augusto <elisiouiux@gmail.com>
2026-03-24 23:09:54 +0100

    make handlers

--- internal/handlers/users_handler.go
+++ internal/handlers/users_handler.go
@@ -1,1 +1,21 @@
 package handlers
+
+import (
+	"go-users-manager-api/internal/service"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserHandler struct {
+	service *service.UserService
+}
+
+func (u *UserHandler) GetUsers(ctx *gin.Context) {
+	users, err := u.service.GetUsers()
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(200, users)
+}
