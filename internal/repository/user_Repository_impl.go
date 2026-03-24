70d38c71ca233bb0b58766edd2232055177b9296
Elisio Augusto <elisiouiux@gmail.com>
2026-03-24 20:37:29 +0100

    user repo

--- internal/repository/user_Repository_impl.go
+++ internal/repository/user_Repository_impl.go
@@ -1,2 +1,20 @@
 package repository
 
+import (
+	"go-users-manager-api/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type GormUserRepository struct {
+	db *gorm.DB
+}
+
+func NewGormUserRepository(db *gorm.DB) *GormUserRepository {
+	return &GormUserRepository{db: db}
+}
+
+func (u *GormUserRepository) Create(usr *models.User) error {
+	return u.db.Create(usr).Error
+}
+
