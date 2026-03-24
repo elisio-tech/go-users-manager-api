67bb9f07c80a224100969dd76fab2587b167b69d
Elisio Augusto <elisiouiux@gmail.com>
2026-03-24 22:57:13 +0100

    service Update

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -20,3 +20,23 @@ func (u *UserService) GetUsers() ([]models.User, error) {
 	}
 	return users, nil
 }
+
+func (u *UserService) GetUserByID(id string) (*models.User, error) {
+	user, err := u.UserRepo.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func (u *UserService) DeleteUser(id string) error {
+	err := u.UserRepo.Delete(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *UserService) UpdateUser(id string, user *models.User) error {
+	return u.UserRepo.Update(id, user)
+}
